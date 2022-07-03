0608a5a59ab17422bf7f203e61e763a537a36892
KuI3uS <kuba.marcinkowski98@wp.pl>
2022-07-03 16:58:19 +0200

    Initial commit

---
+++ src/test/java/pl/pjatk/JakMar/JakMarApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.JakMar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakMarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
